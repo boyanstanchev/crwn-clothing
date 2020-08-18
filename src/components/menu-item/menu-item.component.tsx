00a9d2d3a1002db6c5a62a80bc801025f409a446
Boyan Stanchev <boyan_stan4ev@hotmail.com>
2020-08-18 15:02:31 +0300

    * added prettier config * split components

---
+++ src/components/menu-item/menu-item.component.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './menu-item.styles.scss';
+
+const MenuItem = ({
+  title,
+  imageUrl,
+  size,
+}: {
+  title: string;
+  imageUrl: string;
+  size?: string;
+}) => (
+  <div className={`${size || ''} menu-item`}>
+    <div
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+      className="background-image"
+    ></div>
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
+
+export default MenuItem;
